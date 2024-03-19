bb55c6319928f4ccc0a8b01226fd3512f0bed8b4
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-19 13:32:18 +0530

    added more functionality

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+const mongoose=require("mongoose");
+const connectToDatabase=require("./db");
+require("dotenv").config()
+const bodyparser=require("body-parser")
+const dairyRoutes= require("./routes/dairyRoutes");
+const userRoutes=require("./routes/userRoutes");
+
 const express=require("express");
 const app=express();
-app.get("/" , (req , res)=> {
-    res.send("hi");
-})
-app.listen(3000);
+app.use(bodyparser.json());
+app.use(userRoutes);
+app.use(dairyRoutes);
+//connnecting to the database
+connectToDatabase();
+//starting the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
